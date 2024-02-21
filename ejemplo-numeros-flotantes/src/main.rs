ac69ee750b2977b90d4792eb761906844131e4c1
David Fernández <davidfernanrod@gmail.com>
2024-02-21 20:04:51 +0100

    Fixed exercise

--- ejemplo-numeros-flotantes/src/main.rs
+++ ejemplo-numeros-flotantes/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let x1 = 1.0; // f64
-    let x2: f64 = 2.0; // f64
-    let x3: f32 = 3.0; // f32
+    let x1 = 1.0;
+    let x2: f64 = 2.0;
+    let x3: f32 = 3.0;
     println!("x1: {x1}");
     println!("x2: {x2}");
     println!("x3: {x3}");
