e8ef5ee5dfa1fe1dd1a87de4497f341b108ce1cf
David Fernández <davidfernanrod@gmail.com>
2024-02-21 20:02:20 +0100

    Fixed exercise

--- ejemplo-numeros-enteros/src/main.rs
+++ ejemplo-numeros-enteros/src/main.rs
@@ -2,11 +2,11 @@ fn main() {
     let entero_con_signo_32: i32 = -15;
     let entero_con_signo_64: i64 = 123456789012345;
 
-    let entero_sin_signo_32: u8 = 15;
-    let entero_sin_signo_64: u16 = 12345;
+    let entero_sin_signo_8: u8 = 15;
+    let entero_sin_signo_16: u16 = 12345;
 
     println!("Entero con signo (32 bits): {entero_con_signo_32}");
-    println!("Entero con signo (32 bits): {entero_con_signo_64}");
-    println!("Entero sin signo (64 bits): {entero_sin_signo_32}");
-    println!("Entero sin signo (64 bits): {entero_sin_signo_64}");
+    println!("Entero con signo (64 bits): {entero_con_signo_64}");
+    println!("Entero sin signo (8 bits): {entero_sin_signo_8}");
+    println!("Entero sin signo (16 bits): {entero_sin_signo_16}");
 }
